11bff777d89eb6bbc4aed2e92647cf7e145b2cb3
Jack <jedix1488@gmail.com>
2022-10-25 04:04:46 +0300

    first commit

---
+++ precache-manifest.bc74689a4df36583de1fab48a2c3219b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce8f00ffacf3da7e0035c8689cfdd345",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aa7e7c973e2b4317344d",
+    "url": "/static/js/2.c93619ae.chunk.js"
+  },
+  {
+    "revision": "e293394960b3f4f06cb177456926c36d",
+    "url": "/static/js/2.c93619ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2928d7d1f909942a027d",
+    "url": "/static/js/main.bbf24fb2.chunk.js"
+  },
+  {
+    "revision": "e5593d5421b1fc780b39",
+    "url": "/static/js/runtime-main.cb7faffb.js"
+  }
+]);
